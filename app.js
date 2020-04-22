e1defefb2c328c54181cb881ef2bcdac4a99fe1b
Dheeraj J C <dheerajjc34@gmail.com>
2020-04-22 16:03:58 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+app.get("/service", function(req, res) {
+    res.render("service");
+});
+
+
+app.get("/about", function(req, res) {
+    res.render("about");
+});
+
+app.get("/contact", function(req, res) {
+    res.render("contact");
+});
+
+app.listen(3000, function() {
+    console.log("port is starting at 3000");
+    console.log("server started")
+})
